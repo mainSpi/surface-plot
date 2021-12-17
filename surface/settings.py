366a19f3556624913f09af43abba2f54369f5d4d
mainSpi <spinelimurilo.conf@gmail.com>
2021-12-17 12:20:21 -0300

    aa

--- surface/settings.py
+++ surface/settings.py
@@ -28,7 +28,7 @@ mimetypes.add_type("text/html", ".css", True)
 SECRET_KEY = os.environ['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = []
